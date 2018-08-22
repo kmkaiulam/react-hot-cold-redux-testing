91f17747c1d0e33bf6b963a21afd4aca40a7223c
Kenny Lam <kmlam@ucdavis.edu>
2018-08-22 11:03:44 -0700

    completed actions and reducer tests

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,11 +1,30 @@
 import {GENERATE_AURAL_UPDATE, generateAuralUpdate, RESTART_GAME, restartGame, MAKE_GUESS, makeGuess} from './actions';
 
-describe('restartGame', () => {
 
-    it('should reset the state', () => {
+describe('generateAuralUpdate', () => {
+    
+    it('should have the correct action.type of GENERATE_AURAL_UPDATE', () => {
+        const action = generateAuralUpdate();
+        expect(action.type).toEqual(GENERATE_AURAL_UPDATE);
+    });
+});
+
+describe('restartGame', () => {
+    
+    it('should have the correct action.type of RESTART_GAME and take in correct answer as a parameter', () => {
         const correctAnswer = 100;
         const action = restartGame(correctAnswer);
         expect(action.type).toEqual(RESTART_GAME);
         expect(action.correctAnswer).toEqual(100);
-    })
-})
+    });
+});
+
+describe('makeGuess', () => {
+    
+    it('should have the correct action.type of MAKE_GUESS and take in a guess as a parameter', () => {
+        const guess = 25;
+        const action = makeGuess(guess);
+        expect(action.type).toEqual(MAKE_GUESS);
+        expect(action.guess).toEqual(guess);
+    });
+});
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,19 +1,101 @@
-import {reducer} from './reducer';
-import {restartGame, RESTART_GAME} from './actions';
+import reducer from './reducer';
+import {restartGame, makeGuess, generateAuralUpdate } from './actions';
+
+describe ('Reducer', () => {
+    
+    it('Should return the inital state when nothing is passed in', () => {
+        const state = (reducer(undefined, {type: 'UNKNOWN'}))
+        expect(state.guesses)
+        expect(state.guesses).toEqual([]);
+        expect(state.feedback).toEqual('Make your guess!');
+        expect(state.auralStatus).toEqual('');
+        expect(state.correctAnswer).toBeGreaterThanOrEqual(1);
+        expect(state.correctAnswer).toBeLessThanOrEqual(100);
+    });
+
+    it('Should return the current state if action is not recognized', () => {
+        let currentState = {  
+            guesses: [1, 2, 3, 4],
+            feedback: 'Guess the number!',
+            auralStatus: '',
+            correctAnswer: 55
+        }
+        const state = (reducer(currentState, {type: 'UNKNOWN'}))
+        expect(state).toEqual(currentState);
+    });
+});
 
 describe('restartGame', () => {
-
+    
     it('should reset the state', () => {
         let state = {
             guesses: [1, 2, 3, 4],
             feedback: 'Guess the number!',
             auralStatus: '',
             correctAnswer: 55
-        }
-        state = reducer(state=reducer, restartGame(12));
+        };
+        const correctAnswer = 12;
+        state = reducer(state, restartGame(correctAnswer));
         expect(state.guesses).toEqual([]);
         expect(state.feedback).toEqual('Make your guess!');
         expect(state.auralStatus).toEqual('');
-        expect(state.correctAnswer).toEqual(55);
+        expect(state.correctAnswer).toEqual(correctAnswer);
     });
 });
+
+describe('makeGuess', () => {
+    
+    it('should add the guess to state.guesses', () => {
+        let state = {
+            guesses: [],
+           
+        };
+        let guess1 = 1;
+        let guess2 = 2;
+        state = reducer(state, makeGuess(guess1));
+        state = reducer(state, makeGuess(guess2));
+        expect(state.guesses).toEqual([guess1, guess2]); 
+    });
+
+    it('should return the appropriate feedback when a guess is made', () => {
+        let state = {
+            guesses: [],
+            feedback: 'Make your guess!',
+            auralStatus: '',
+            correctAnswer: 100,
+        };
+        state = reducer(state, makeGuess('abc'));
+        expect(state.feedback).toEqual('Please enter a valid number.');
+        state = reducer(state, makeGuess(50));
+        expect(state.feedback).toEqual("You're Ice Cold...");
+        state = reducer(state, makeGuess(70));
+        expect(state.feedback).toEqual("You're Cold...");
+        state = reducer(state, makeGuess(90));
+        expect(state.feedback).toEqual("You're Warm.");
+        state = reducer(state, makeGuess(99));
+        expect(state.feedback).toEqual("You're Hot!");
+        state = reducer(state, makeGuess(100));
+        expect(state.feedback).toEqual("You got it!");
+    });
+});
+
+
+describe('generateAuralUpdate', () => {
+    
+    it('should modify state.auralStatus with the current feedback and guesses', () => {
+        let state = {
+            guesses: [],
+            feedback: 'Make your guess!',
+            auralStatus: '',
+            correctAnswer: 55
+        };
+        let guess1 = 1;
+        let guess2 = 2;
+        state = reducer(state, makeGuess(guess1));
+        state = reducer(state, makeGuess(guess2));
+        state = reducer(state, generateAuralUpdate())
+        expect(state.auralStatus).toEqual("Here's the status of the game right now: You're Ice Cold... You've made 2 guesses. In order of most- to least-recent, they are: 2, 1");
+        expect(state.guesses).toEqual([guess2, guess1]); 
+        expect(state.correctAnswer).toEqual(55);
+    })
+})
